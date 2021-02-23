d77324643478006358034817b2989ea36ba49d55
Layik Hama <layik.hama@gmail.com>
2021-02-23 11:46:54 +0000

    Add bare eatlas

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
+import React from 'react';
+import Eatlas from 'eatlas';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>Hello eAtlas</p>
-      </header>
-    </div>
+    <Eatlas />
   );
 }
 
