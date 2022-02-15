234055a6ccf0d340fbdb862888e83d21666fc014
Layik Hama <layik.hama@gmail.com>
2022-02-15 23:41:09 +0000

    Dockerfile.R no geospatial deps + .dockerignore + minor plumber.R

--- Dockerfile.R
+++ Dockerfile.R
@@ -1,45 +1,16 @@
 FROM rstudio/r-base:4.0-focal
 
-RUN apt-get update \ 
-	&& apt-get install -y --no-install-recommends \
-		software-properties-common \
-    ed \
-		less \
-		locales \
-		vim-tiny \
-		wget \
-		ca-certificates \
-    && apt-get update 
-
 RUN apt-get update \
   && apt-get install -y --no-install-recommends \
-    lbzip2 \
-    libfftw3-dev \
-    libgdal-dev \
-    libgeos-dev \
-    libgsl0-dev \
-    libgl1-mesa-dev \
-    libglu1-mesa-dev \
-    libhdf4-alt-dev \
-    libhdf5-dev \
-    libproj-dev \
-    libprotobuf-dev \
-    libnetcdf-dev \
-    libsqlite3-dev \
     # required for plumber
     libsodium-dev \ 
     libssl-dev \
     libudunits2-dev \
-    netcdf-bin \
-    tk-dev \
-    unixodbc-dev \
-    libv8-dev \
-    protobuf-compiler \ 
-    git
+    netcdf-bin
 
 # RUN apt-get install -y r-cran-devtools r-cran-sf r-cran-plumber
 
-RUN R -e 'install.packages(c("geojsonsf", "devtools", "sf", "curl", "plumber"), repos="http://cran.us.r-project.org")'
+RUN R -e 'install.packages(c("plumber"), repos="http://cran.us.r-project.org")'
 # RUN R -e 'devtools::install_github("ATFutures/geoplumber")'
 
 # add node/npm
@@ -51,7 +22,7 @@ ADD . /app
 # build
 WORKDIR /app
 RUN npm install
-RUN npm run build
+RUN npm run tgver
 RUN rm -rf node_modules
 
 EXPOSE 8000
