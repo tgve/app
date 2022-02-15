d3b52a5bb0a43ae2b16d095c9af33e9edbf36d6e
Roly Perera <roly.perera@dynamicaspects.org>
2022-02-15 10:13:41 +0000

    Install Flask in virtualenv.

--- Python/tgve.py
+++ Python/tgve.py
@@ -1,15 +1,20 @@
 import os
 from flask import Flask, send_from_directory
 
-app = Flask(__name__, static_folder='build')
+app = Flask(__name__, static_folder='../build')
 
 # Serve React App
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
 def serve(path):
+    print(path)
+    print(app.static_folder)
+    print(os.path.exists(app.static_folder))
     if path != "" and os.path.exists(app.static_folder + '/' + path):
+        print("Path branch")
         return send_from_directory(app.static_folder, path)
     else:
+        print("index.html branch")
         return send_from_directory(app.static_folder, 'index.html')
 
 
