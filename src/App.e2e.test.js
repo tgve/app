683b588181cb8dcfe8911b2db6e16e86e17f98a9
Layik Hama <layik.hama@gmail.com>
2022-03-15 07:30:17 +0000

    e2e tests running local build

--- src/App.e2e.test.js
+++ src/App.e2e.test.js
@@ -1,27 +1,43 @@
 import puppeteer from "puppeteer";
+const url = require('url');
+const fs = require("fs")
+
+let browser;
+let page;
+
+beforeAll(async () => {
+    browser = await puppeteer.launch();
+    page = await browser.newPage();
+});
 
 describe("App.js", () => {
-    let browser;
-    let page;
-
-    beforeAll(async () => {
-        browser = await puppeteer.launch();
-        page = await browser.newPage();
-    });
-
+    if(!fs.existsSync("build")) {
+        console.log("No build found. yarn run build-local first")
+        return; //skip
+    }
     it("contains the welcome text", async () => {
-        await page.goto("http://localhost:3000");
+        await page.goto(url.pathToFileURL("build/index.html"));
         await page.waitForSelector(".side-pane-header");
         const text = await page.$eval("h2", (e) => e.textContent);
-        expect(text).toContain("Nothing to show");
+        expect(text).toContain("Nothing to show")
     });
 
-    it("contains the welcome text", async () => {
-        await page.goto("http://localhost:3000?defaultURL=https://raw.githubusercontent.com/tgve/example-data/main/casualties_100.geojson");
+    it("contains the 100 rows", async () => {
+        await page.goto(url.pathToFileURL("build/index.html")
+            + "?defaultURL=https://raw.githubusercontent.com/tgve/example-data/main/casualties_100.geojson");
         await page.waitForSelector(".side-pane-header");
         const text = await page.$eval("h2", (e) => e.textContent);
-        expect(text).toContain("100 rows");
+        expect(text).toContain("100 rows")
+
     });
 
-    afterAll(() => browser.close());
+    it("wrong url includes Nothing to show", async () => {
+        await page.goto(url.pathToFileURL("build/index.html")
+            + "?defaultURL=https://rongurl.fail");
+        await page.waitForSelector(".side-pane-header");
+        const text = await page.$eval("h2", (e) => e.textContent);
+        expect(text).toContain("Nothing to show")
+    });
+
 })
+afterAll(async () => browser.close());
