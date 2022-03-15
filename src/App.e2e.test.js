cd8f79a1ec4de2579f4f08808064c27b71ab8d1a
LydiaFrance <lfrance@turing.ac.uk>
2022-03-15 14:28:17 +0000

    Add a screenshot snapshot test and waiting for loading to finish in the tests

--- src/App.e2e.test.js
+++ src/App.e2e.test.js
@@ -5,7 +5,7 @@ import { toMatchImageSnapshot } from 'jest-image-snapshot'
 
 export function setConfig(filename) {
     return {
-        failureThreshold: '0.01',
+        failureThreshold: '0',
         failureThresholdType: 'percent',
         customSnapshotsDir: `${__dirname}/../__snapshots__/`,
         customSnapshotIdentifier: filename,
@@ -28,27 +28,54 @@ describe("App.js", () => {
     }
 
     it("no url includes Nothing to show", async () => {
-        await page.goto(url.pathToFileURL("build/index.html"));
+        await page.goto(url.pathToFileURL("build/index.html"),{
+            waitUntil: "networkidle0",
+        });
         await page.waitForSelector(".side-pane-header");
-        const text = await page.$eval("h2", (e) => e.textContent);
+        const text = await page.$eval(".side-pane-header > h2", (e) => e.textContent);
         expect(text).toContain("Nothing to show")
     });
 
     it("contains the 100 rows", async () => {
         await page.goto(url.pathToFileURL("build/index.html")
-            + "?defaultURL=https://raw.githubusercontent.com/tgve/example-data/main/casualties_100.geojson");
+            + "?defaultURL=https://raw.githubusercontent.com/tgve/example-data/main/casualties_100.geojson",{
+                waitUntil: "networkidle0",
+            });
         await page.waitForSelector(".side-pane-header");
-        const text = await page.$eval("h2", (e) => e.textContent);
+        const text = await page.$eval(".side-pane-header > h2", (e) => e.textContent);
         expect(text).toContain("100 rows")
     });
 
     it("wrong url includes Nothing to show", async () => {
         await page.goto(url.pathToFileURL("build/index.html")
-            + "?defaultURL=https://rongurl.fail");
+            + "?defaultURL=https://rongurl.fail",{
+                waitUntil: "networkidle0",
+            });
         await page.waitForSelector(".side-pane-header");
-        const text = await page.$eval("h2", (e) => e.textContent);
+        const text = await page.$eval(".side-pane-header > h2", (e) => e.textContent);
         expect(text).toContain("Nothing to show")
     });
+
+    it("check screenshot", async () => {
+        await page.goto(url.pathToFileURL("build/index.html"), {
+            waitUntil: "networkidle0",
+            });
+        await page.waitForSelector(".side-pane-header");  
+        const image = await page.screenshot({ fullPage: true }); 
+        expect(image).toMatchImageSnapshot(setConfig('image-name'));
+    });
+
+    
 })
 
+
+
+
 afterAll(async () => browser.close());
+
+/*
+const image = await page.screenshot({ fullPage: true });
+expect(image).toMatchImageSnapshot(setConfig('image-name'));
+}, 15000);
+
+*/
