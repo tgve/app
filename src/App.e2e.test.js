78a1630bd651250b2666ae5d99fc3f11da393b8f
LydiaFrance <lfrance@turing.ac.uk>
2022-03-15 16:28:44 +0000

    Move snapshot folder and remove idle wait in tests

--- src/App.e2e.test.js
+++ src/App.e2e.test.js
@@ -29,7 +29,7 @@ describe("App.js", () => {
 
     it("no url includes Nothing to show", async () => {
         await page.goto(url.pathToFileURL("build/index.html"),{
-            waitUntil: "networkidle0",
+            //waitUntil: "networkidle0",
         });
         await page.waitForSelector(".side-pane-header");
         const text = await page.$eval(".side-pane-header > h2", (e) => e.textContent);
@@ -39,7 +39,7 @@ describe("App.js", () => {
     it("contains the 100 rows", async () => {
         await page.goto(url.pathToFileURL("build/index.html")
             + "?defaultURL=https://raw.githubusercontent.com/tgve/example-data/main/casualties_100.geojson",{
-                waitUntil: "networkidle0",
+                //waitUntil: "networkidle0",
             });
         await page.waitForSelector(".side-pane-header");
         const text = await page.$eval(".side-pane-header > h2", (e) => e.textContent);
@@ -49,7 +49,7 @@ describe("App.js", () => {
     it("wrong url includes Nothing to show", async () => {
         await page.goto(url.pathToFileURL("build/index.html")
             + "?defaultURL=https://rongurl.fail",{
-                waitUntil: "networkidle0",
+                //waitUntil: "networkidle0",
             });
         await page.waitForSelector(".side-pane-header");
         const text = await page.$eval(".side-pane-header > h2", (e) => e.textContent);
@@ -58,7 +58,7 @@ describe("App.js", () => {
 
     it("check screenshot", async () => {
         await page.goto(url.pathToFileURL("build/index.html"), {
-            waitUntil: "networkidle0",
+            //waitUntil: "networkidle0",
             });
         await page.$eval('.mapboxgl-map',e => e.setAttribute("style", "visibility: hidden"));
         await page.$eval('.loader',e => e.setAttribute("style", "visibility: hidden"));
