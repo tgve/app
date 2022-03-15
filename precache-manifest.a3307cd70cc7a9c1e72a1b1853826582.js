9aaa66e0514377b71d913f347fc547e8afda38e2
rolyp <rolyp@users.noreply.github.com>
2022-03-15 09:22:38 +0000

    deploy: 495f67d4bb1b7e19562761d7bc4b218de9997469

---
+++ precache-manifest.a3307cd70cc7a9c1e72a1b1853826582.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04ab363f3c0b8a1f2e20cd14538f00a9",
+    "url": "./index.html"
+  },
+  {
+    "revision": "6eee7ca7a2bf7f4d9dd7",
+    "url": "./static/css/2.c3876fe1.chunk.css"
+  },
+  {
+    "revision": "8fb87b260c684e2424b9",
+    "url": "./static/css/main.5e3647ae.chunk.css"
+  },
+  {
+    "revision": "6eee7ca7a2bf7f4d9dd7",
+    "url": "./static/js/2.05ea639a.chunk.js"
+  },
+  {
+    "revision": "9aa61635b463114b9e9ffea45bde072b",
+    "url": "./static/js/2.05ea639a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f906d17a045dc74735db",
+    "url": "./static/js/3.c62b9eb5.chunk.js"
+  },
+  {
+    "revision": "8fb87b260c684e2424b9",
+    "url": "./static/js/main.b08700b4.chunk.js"
+  },
+  {
+    "revision": "fd083d600da17114441e",
+    "url": "./static/js/runtime-main.986a0626.js"
+  },
+  {
+    "revision": "3793283389a32fd45961ac33577d9316",
+    "url": "./static/media/location-icon-atlas.37932833.png"
+  }
+]);
