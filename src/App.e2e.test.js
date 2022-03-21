94f698923ac1dd4b059e6706174be243f7275616
LydiaFrance <lfrance@turing.ac.uk>
2022-03-21 16:01:31 +0000

    End each test with return

--- src/App.e2e.test.js
+++ src/App.e2e.test.js
@@ -47,19 +47,19 @@ describe("App.js", () => {
 
     it("no url includes Nothing to show", async () => {
         await page.goto(url.pathToFileURL("build/index.html"));
-        await waitForElementText("Nothing to show",'.side-pane-header > h2')
+        return waitForElementText("Nothing to show",'.side-pane-header > h2')
     });
 
     it("contains 100 rows", async () => {
         await page.goto(url.pathToFileURL("build/index.html")
             + "?defaultURL=https://raw.githubusercontent.com/tgve/example-data/main/casualties_100.geojson");
-        await waitForElementText("100 rows",'.side-pane-header > h2')
+        return waitForElementText("100 rows",'.side-pane-header > h2')
     });
 
     it("wrong url includes Nothing to show", async () => {
         await page.goto(url.pathToFileURL("build/index.html")
             + "?defaultURL=https://wrongurl.fail");
-        await waitForElementText("Nothing to show",'.side-pane-header > h2')
+        return waitForElementText("Nothing to show",'.side-pane-header > h2')
     });
 
     it("check screenshot", async () => {
