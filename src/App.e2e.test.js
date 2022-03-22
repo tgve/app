e1475f5304c6403f5eb85530ebc5f6451bdabcd0
Roly Perera <roly.perera@dynamicaspects.org>
2022-03-22 16:19:15 +0000

    Merge other branch.

--- src/App.e2e.test.js
+++ src/App.e2e.test.js
@@ -15,73 +15,89 @@ export function setConfig() {
     }
 }
 
-async function waitForElementText(text,selector) {
-    return page.waitForFunction(
+// Wait until at least one element matches selector and has specified text.
+async function waitForElementText(selector, text) {
+    await page.waitForFunction(
         (selector, text) => {
-            const e = document.querySelector(selector)
-            return e && e.textContent == text
+            const es = [...document.querySelectorAll(selector)]
+            console.log(`Found ${es.length} nodes matching ${selector}`)
+            return es.some(e => e.textContent == text)
         },
-        {timeout:30000},
+        { timeout: 30000 },
         selector,
         text
-    );
+    )
 }
 
 async function screenshot() {
-    await page.$eval('.mapboxgl-map',e => e.setAttribute("style", "visibility: hidden"));
-    await page.$eval('.loader',e => e.setAttribute("style", "visibility: hidden"));
+    await page.$eval('.mapboxgl-map', e => e.setAttribute("style", "visibility: hidden"));
+    await page.$eval('.loader', e => e.setAttribute("style", "visibility: hidden"));
     return page.screenshot({ fullPage: true });
 }
-
 
 let browser;
 let page;
 
 beforeAll(async () => {
-    browser = await puppeteer.launch();
+    browser = await puppeteer.launch({
+        dumpio: false // if true then formidable amount of console logging
+    });
     page = await browser.newPage();
     await page.setViewport({ width: 800, height: 1400 });
     expect.extend({ toMatchImageSnapshot });
 });
 
+afterAll(async () => browser.close());
+
 describe("App.js", () => {
-    if(!fs.existsSync("build")) {
+    if (!fs.existsSync("build")) {
         fail("No build found; 'yarn build-local' first")
     }
 
-    it("no url includes Nothing to show", async () => {
+    it("no URL: includes Nothing to show", async () => {
         await page.goto(url.pathToFileURL("build/index.html"));
-        return waitForElementText("Nothing to show",'.side-pane-header > h2')
+        return waitForElementText('.side-pane-header > h2', "Nothing to show")
     });
 
-    it("contains 100 rows", async () => {
+    it("casualties_100: includes 100 rows", async () => {
         await page.goto(url.pathToFileURL("build/index.html")
             + "?defaultURL=https://raw.githubusercontent.com/tgve/example-data/main/casualties_100.geojson");
-        return waitForElementText("100 rows",'.side-pane-header > h2')
+        return waitForElementText('.side-pane-header > h2', "100 rows")
     });
 
-    it("wrong url includes Nothing to show", async () => {
+    it("wrong URL: includes Nothing to show", async () => {
         await page.goto(url.pathToFileURL("build/index.html")
             + "?defaultURL=https://wrongurl.fail");
-        return waitForElementText("Nothing to show",'.side-pane-header > h2')
+        return waitForElementText('.side-pane-header > h2', "Nothing to show")
     });
 
     it("check screenshot", async () => {
         await page.goto(url.pathToFileURL("build/index.html"));
-        await waitForElementText("Nothing to show",'.side-pane-header > h2')
+        await waitForElementText('.side-pane-header > h2', "Nothing to show")
         const image = await screenshot();
         expect(image).toMatchImageSnapshot(setConfig());
     });
 
     it("check screenshot with data uploaded", async () => {
         await page.goto(url.pathToFileURL("build/index.html")
-        + "?defaultURL=https://raw.githubusercontent.com/tgve/example-data/main/casualties_100.geojson");
-        await waitForElementText("100 rows",'.side-pane-header > h2')
+            + "?defaultURL=https://raw.githubusercontent.com/tgve/example-data/main/casualties_100.geojson");
+        await waitForElementText('.side-pane-header > h2', "100 rows")
         const image = await screenshot();
         expect(image).toMatchImageSnapshot(setConfig());
     });
 
+    it("check screenshot with filter", async () => {
+        await page.goto(url.pathToFileURL("build/index.html")
+            + "?defaultURL=https://raw.githubusercontent.com/tgve/example-data/main/casualties_100.geojson")
+        await waitForElementText('.side-pane-header > h2', "100 rows")
+        await waitForElementText('.side-panel-body-content > div > span', "Slight")
+
+        const xp = "//div[contains(@class, 'side-panel-body-content')]//div"
+            + "/span[contains(text(),'Slight')]/.."
+        const [e] = await page.$x(xp)
+        await e.click()
+
+        const image = await screenshot()
+        expect(image).toMatchImageSnapshot(setConfig())
+    })
 })
-
-
-afterAll(async () => browser.close());
