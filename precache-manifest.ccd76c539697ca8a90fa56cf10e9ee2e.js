01998ff6f65b364a8259b517d5cd5520687e8357
rolyp <rolyp@users.noreply.github.com>
2022-03-29 09:32:46 +0000

    deploy: 2cdce2bb29ee5ea425ae4ca555b1d33efcf69946

---
+++ precache-manifest.ccd76c539697ca8a90fa56cf10e9ee2e.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36ec1f9d51e2057c9124bbdb76c71ba9",
+    "url": "https://tgve.github.io/app/index.html"
+  },
+  {
+    "revision": "ab974ff8c818b67a2016",
+    "url": "https://tgve.github.io/app/static/css/2.231d0962.chunk.css"
+  },
+  {
+    "revision": "dc5f23372b332638b05b",
+    "url": "https://tgve.github.io/app/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "ab974ff8c818b67a2016",
+    "url": "https://tgve.github.io/app/static/js/2.aa5e6f79.chunk.js"
+  },
+  {
+    "revision": "9aa61635b463114b9e9ffea45bde072b",
+    "url": "https://tgve.github.io/app/static/js/2.aa5e6f79.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f906d17a045dc74735db",
+    "url": "https://tgve.github.io/app/static/js/3.c62b9eb5.chunk.js"
+  },
+  {
+    "revision": "dc5f23372b332638b05b",
+    "url": "https://tgve.github.io/app/static/js/main.20f8f99d.chunk.js"
+  },
+  {
+    "revision": "01f8803c13334bf742be",
+    "url": "https://tgve.github.io/app/static/js/runtime-main.c385f11e.js"
+  },
+  {
+    "revision": "3793283389a32fd45961ac33577d9316",
+    "url": "https://tgve.github.io/app/static/media/location-icon-atlas.37932833.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/app/precache-manifest.ec565469bf33ca662d47a3c93fb065a7.js"
+  "https://tgve.github.io/app/precache-manifest.ccd76c539697ca8a90fa56cf10e9ee2e.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/app/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("https://tgve.github.io/app/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
