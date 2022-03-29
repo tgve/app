ecbf12bd77e74e112e8604d342f3002e937453ba
Roly Perera <roly.perera@dynamicaspects.org>
2022-03-29 17:08:29 +0100

    Remove 'test', 'Test'.

--- src/App.test.js
+++
@@ -1,9 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-import React from 'react';
-
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/add data/i);
-  expect(linkElement).toBeInTheDocument();
-});
